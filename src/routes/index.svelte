0bede265af0e43f9b4be45771712ebabfc0c2a1b
0xedb <edohbruno@gmail.com>
2021-06-12 00:53:31 +0000

    project init

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<script lang="typescript">
+	let count = 0;
+
+</script>
+
+<h1 on:click={() => ++count}>{count}</h1>
+
+<style></style>
